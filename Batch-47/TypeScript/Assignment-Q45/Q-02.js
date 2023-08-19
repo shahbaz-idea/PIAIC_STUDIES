48c7f0598d5d4c586e941ed6c69d47b84d888daa
Shahbaz Akbar <shahbaz.idea@outlook.com>
2023-08-19 20:13:28 +0500

    Q01

---
+++ Batch-47/TypeScript/Assignment-Q45/Q-02.js
@@ -0,0 +1,2 @@
+var sName = "Zain";
+console.log("Hello ".concat(sName, ", would you like to learn some Python today?"));
---
+++ Batch-47/TypeScript/Assignment-Q45/Q-02.ts
@@ -0,0 +1,2 @@
+let sName:string = "Zain"
+console.log(`Hello ${sName}, would you like to learn some Python today?`);
