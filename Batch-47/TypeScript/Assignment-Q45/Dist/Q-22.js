72f40f2e412daca1c6fce516d6d5d16e52a7f1e6
Shahbaz Akbar <shahbaz.idea@outlook.com>
2023-08-27 00:22:54 +0500

    Q22

---
+++ Batch-47/TypeScript/Assignment-Q45/Dist/Q-22.js
@@ -0,0 +1,12 @@
+"use strict";
+let lstArrsData = ["Ravi", "Satluj", "Sindh", "Jehlem", "Chinab"];
+console.log("=================");
+for (let index = 0; index < lstArrsData.length + 1; index++) {
+    const element = lstArrsData[index];
+    console.log(element);
+}
+console.log("=================");
+for (let index = 0; index < lstArrsData.length; index++) {
+    const element = lstArrsData[index];
+    console.log(element);
+}
---
+++ Batch-47/TypeScript/Assignment-Q45/Q-22.ts
@@ -0,0 +1,15 @@
+
+let lstArrsData:Array<string> = ["Ravi","Satluj","Sindh","Jehlem","Chinab"];
+
+
+console.log("=================");
+for (let index = 0; index < lstArrsData.length+1; index++) {
+    const element = lstArrsData[index];
+    console.log(element);
+}
+
+console.log("=================");
+for (let index = 0; index < lstArrsData.length; index++) {
+    const element = lstArrsData[index];
+    console.log(element);
+}
