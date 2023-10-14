ce2c13e79d9f291382b552d9148fea7af943d896
Shahbaz Akbar <shahbaz.idea@outlook.com>
2023-10-15 03:22:17 +0500

    numb

---
+++ Batch-47/TypeScript/Node_Projects/Number_Guessing/dist/index.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "number",
+        name: "age",
+        message: "What is your age?"
+    },
+    {
+        type: "list",
+        name: "gender",
+        message: "What is your gender?",
+        choices: ["Male", "Female"]
+    }
+]);
+console.log("Your Name is " + answer.name);
+console.log("Your Age is " + answer.age);
+console.log("Your Gender is " + answer.gender);
